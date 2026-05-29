83d347cf345a7aee1f12e48055f0d11516dfeb71
María José V.N <mariajose.valderrama.n@gmail.com>
2026-05-28 20:16:11 -0400

    detalles hasta función boxM()

--- Código Practico.R
+++ Código Practico.R
@@ -128,14 +128,16 @@ ggpairs(datos_pair,
 
 
 
+
+
 #Inferencia Multivariada----
 ##Test de normalidad multivariada----
 library(MVN)
 
 mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "SW")
 mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "Lillie") #Se rechaza de manera univariada EDAD
-mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "AD") #Se rechaza de manera univariada EDAD
-mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "CVM") #Se rechaza de manera univariada EDAD
+mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "AD")     #Se rechaza de manera univariada EDAD
+mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "CVM")    #Se rechaza de manera univariada EDAD
 
 
 ##Dos muestras aleatorias homogéneas e independientes (Comparación de medias)----
@@ -150,13 +152,14 @@ Sexo <- Base_Multi$Sexo
 # Ejecutamos el test comparando Hombres vs Mujeres con todos los datos
 HotellingsT2Test(vec[Sexo == "HOMBRE", ], vec[Sexo == "MUJER", ])
 
+
 ##K (3) muestras aleatorias homogéneas e independientes(Comparación de medias MANOVA)----
 
 #Creamos el modelo MANOVA
 modelo_manova <- manova(cbind(Edad, Peso, IMC, Presión_Arterial_Sistólica) ~ Estado_Nutricional, data = Base_Multi)
 
-summary(modelo_manova, test = "Wilks") #Este es el que normalmente en el curso de utilizaría 
-summary(modelo_manova, test = "Pillai") #Dice que es uno más riguroso que Wilks
+summary(modelo_manova, test = "Wilks")             #Este es el que normalmente en el curso de utilizaría 
+summary(modelo_manova, test = "Pillai")            #Dice que es uno más riguroso que Wilks
 summary(modelo_manova, test = "Hotelling-Lawley")
 summary(modelo_manova, test = "Roy")
 
@@ -164,10 +167,12 @@ summary(modelo_manova, test = "Roy")
 ##Homogeneidad de matrices de varianza-covarianza (M de Box)----
 
 #install.packages("biotools")
+
 library(biotools)
 
 #Para grupos por sexo:
 boxM(vec, Base_Multi$Sexo)
+
 #Para grupos por estado nutricional:
 boxM(vec, Base_Multi$Estado_Nutricional)
 
